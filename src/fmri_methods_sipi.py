5d2b4d962828c001289c608adaa130d36f721e9b
Anand A Joshi <ajoshi@sipi.usc.edu>
2016-08-14 00:42:40 -0700

    hotelling test is added. PCA based analysis is added.

--- src/fmri_methods_sipi.py
+++ src/fmri_methods_sipi.py
@@ -19,11 +19,11 @@ def rot_sub_data(ref,sub):
 
 
 def show_slices(slices,vmax=None,vmin=None):
-       """ Function to display row of image slices """
+    """ Function to display row of image slices """
     fig, axes = plt.subplots(1, len(slices))
     for i, slice in enumerate(slices):
-        axes[i].imshow(slice.T, cmap="gray", origin="lower",vmax=vmax,vmin=vmin)
-    plt.colorbar()
+        a = axes[i].imshow(slice.T, cmap="gray", origin="lower",vmax=vmax,vmin=vmin)
+    fig.colorbar(a)
 
 
 def reorder_labels(labels):
@@ -46,4 +46,29 @@ def reorder_labels(labels):
         
     return labels
         
+import numpy as np
+from scipy.stats import f as f_distrib
+
+def hotelling_t2(X,Y):
+    """X and Y are n_features x n_subjects x n_voxels"""
+    nx=X.shape[1];ny=Y.shape[1];p=X.shape[0];
+    Xbar=X.mean(1);Ybar=Y.mean(1);
+    Xbar=Xbar.reshape(Xbar.shape[0],1,Xbar.shape[1])
+    Ybar=Ybar.reshape(Ybar.shape[0],1,Ybar.shape[1])
+
+    X_Xbar=X-Xbar
+    Y_Ybar=Y-Ybar
+    Wx=np.einsum('ijk,ljk->ilk',X_Xbar,X_Xbar)
+    Wy=np.einsum('ijk,ljk->ilk',Y_Ybar,Y_Ybar)
+    W=(Wx+Wy)/float(nx+ny-2)
+    Xbar_minus_Ybar=Xbar-Ybar
+    x = np.linalg.solve(W.transpose(2,0,1), Xbar_minus_Ybar.transpose(2,0,1))
+    x=x.transpose(1,2,0);
+
+    t2=np.sum(Xbar_minus_Ybar*x,0)
+    t2=t2*float(nx*ny)/float(nx+ny);
+    stat=(t2*float(nx+ny-1-p)/(float(nx+ny-2)*p));
+
+    pval=1-np.squeeze(f_distrib.cdf(stat,p,nx+ny-1-p));
+    return pval,t2
         
--- src/main_adhd.py
+++ src/main_adhd.py
@@ -3,21 +3,21 @@
 # First we load the ADHD200 data
 from nilearn import datasets
 import scipy as sp
-from fmri_methods_sipi import rot_sub_data, show_slices
+from fmri_methods_sipi import rot_sub_data, hotelling_t2
 import matplotlib.pyplot as plt
 from nilearn import input_data
-from nilearn.plotting import plot_roi, plot_epi, show
+from nilearn.plotting import plot_roi, show, plot_stat_map
 from nilearn.image.image import mean_img
 from nilearn.image import index_img
+from sklearn.decomposition import PCA
 
-
-adhd_dataset = datasets.fetch_adhd(n_subjects=30)
+adhd_dataset = datasets.fetch_adhd(n_subjects=60)
 func_filenames = adhd_dataset.func  # list of 4D nifti files for each subject
 print('First functional nifti image (4D) is at: %s' %
       func_filenames[0])  # 4D data
 nifti_masker = input_data.NiftiMasker(standardize=True, mask_strategy='epi',
                            memory="nilearn_cache", memory_level=2,
-                           smoothing_fwhm=6*4, detrend=True)
+                           smoothing_fwhm=6, detrend=True)
 # Compute EPI based mask 
 nifti_masker.fit(func_filenames[0])
 
@@ -32,29 +32,74 @@ all_data=sp.zeros((ref_mskd.shape[0],ref_mskd.shape[1],len(func_filenames)))
 all_data[:,:,0]=ref_mskd
 all_data_orig=all_data.copy()
 # Load all the data and rotate it
+adhd_flag=sp.zeros(len(func_filenames))
+adhd_flag[0]=adhd_dataset.phenotypic[0][22]
 for ind in range(1,len(func_filenames)):
     nii_img = index_img(func_filenames[ind], slice(0, 75))
     sub_mskd = nifti_masker.transform(nii_img).T ##check fwhm
     temp = rot_sub_data(ref_mskd, sub_mskd)
     all_data[:,:,ind] = temp
     all_data_orig[:,:,ind] = sub_mskd
+    adhd_flag[ind]=adhd_dataset.phenotypic[ind][22]
     print ind,
     
 
 # Variance before rotation
 var_before = sp.average(sp.var(all_data_orig,axis=2),axis=1)
 var_before=nifti_masker.inverse_transform(var_before)
-var_before=var_before.get_data()
-show_slices([var_before[:,:,30],var_before[:,30,:],var_before[30,:,:]],vmax=1.0,vmin=0.0)
+plot_stat_map(var_before,title='Variance before rotation')
 
 
 # variance after rotation
 var_after = sp.average(sp.var(all_data,axis=2),axis=1)
 var_after=nifti_masker.inverse_transform(var_after)
-var_after=var_after.get_data()
-show_slices([var_after[:,:,30],var_after[:,30,:],var_after[30,:,:]],vmax=1.0,vmin=0.0)
+plot_stat_map(var_after,title='Variance after rotation')
 
-plt.show()
+
+var_after_adhd = sp.average(sp.var(all_data[:,:,adhd_flag>0],axis=2),axis=1) 
+var_after_normal = sp.average(sp.var(all_data[:,:,adhd_flag==0],axis=2),axis=1) 
+var_after=var_after_adhd-var_after_normal
+var_after=nifti_masker.inverse_transform(var_after)
+plot_stat_map(var_after,title='Variance Diff between adhd and normals after rotation')
+
+show()
+# Dimensionality reduction using PCA
+
+n_components=5
+P=PCA(n_components=n_components, whiten=True, copy=True)
+P.fit(all_data[:,:,0])
+all_data_pca=sp.zeros((all_data.shape[0],n_components,all_data.shape[2]))
+print("Doing PCA on Synced data")
+for ind in range(all_data.shape[2]):
+    all_data_pca[:,:,ind]=P.transform(all_data[:,:,ind])
+    print ind,
+
+P.fit(all_data_orig[:,:,0])
+all_data_orig_pca=sp.zeros((all_data_orig.shape[0],n_components,all_data_orig.shape[2]))
+print("Doing PCA on Synced data")
+for ind in range(all_data.shape[2]):
+    all_data_orig_pca[:,:,ind]=P.transform(all_data_orig[:,:,ind])
+    print ind,
 
 # Now lets work on group difference
+print("Doing Group Diff using Hotelling test")
+#pval,t2=hotelling_t2(all_data[:,:,adhd_flag>0].transpose(1,2,0),all_data[:,:,adhd_flag==0].transpose(1,2,0))
+pval,t2=hotelling_t2(all_data[:,:,adhd_flag==0].transpose(1,2,0),all_data[:,:,adhd_flag>0].transpose(1,2,0))
+pval1=nifti_masker.inverse_transform((0.05-pval)*(pval<0.05))
+plot_stat_map(pval1,title='Hotelling test after rotation')
+
+pval,t2=hotelling_t2(all_data_orig[:,:,adhd_flag==0].transpose(1,2,0),all_data_orig[:,:,adhd_flag>0].transpose(1,2,0))
+pval1=nifti_masker.inverse_transform((0.05-pval)*(pval<0.05))
+plot_stat_map(pval1,title='Hotelling test before rotation')
+
+pval,t2=hotelling_t2(all_data_orig_pca[:,:,adhd_flag==0].transpose(1,2,0),all_data_orig_pca[:,:,adhd_flag>0].transpose(1,2,0))
+pval1=nifti_masker.inverse_transform((0.05-pval)*(pval<0.05))
+plot_stat_map(pval1,title='Hotelling test before rotation (PCA)')
+
+
+pval,t2=hotelling_t2(all_data_pca[:,:,adhd_flag==0].transpose(1,2,0),all_data_pca[:,:,adhd_flag>0].transpose(1,2,0))
+pval1=nifti_masker.inverse_transform((0.05-pval)*(pval<0.05))
+plot_stat_map(pval1,title='Hotelling test after rotation (PCA)')
+
+
     
