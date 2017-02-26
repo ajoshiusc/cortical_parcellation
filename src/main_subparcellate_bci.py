75ee1e7f9801dffef0f63ffa8f1a8ebba27c42f6
Anand A Joshi <ajoshi@usc.edu>
2017-02-26 02:00:37 -0800

    code working for mapping subdivisions

--- src/main_subparcellate_bci.py
+++ src/main_subparcellate_bci.py
@@ -28,10 +28,9 @@ p_dir = '/home/sgaurav/Documents/git_sandbox/cortical_parcellation/src/\
 intensity_mode_map/'
 nSub = 40
 lst = os.listdir(p_dir)
-bci_bst = readdfs('/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
-mid.cortex.dfs')
-bci_bst_sm = readdfs('/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.\
-left.mid.cortex_smooth10.dfs')
+bci_bst = readdfs('100307.BCI2reduce3.very_smooth.left.dfs')
+bci_bst_sm = readdfs('100307.BCI2reduce3.very_smooth.left.dfs')
+
 bci_bst.vertices = bci_bst_sm.vertices
 
 freq = sp.zeros(bci_bst.vertices.shape[0])
@@ -49,13 +48,14 @@ for fname in lst:
 
     data1 = scipy.io.loadmat(os.path.join(p_dir, fname))
 
-    labs = data1['labs_all']
-    freq1 = data1['freq']
+    labs = data1['labs_all'].squeeze()
+    freq1 = data1['freq'].squeeze()
     freq1 = freq1/nSub
     freq1[labs == 0] = 0
-    bci_labs = reduce3_to_bci_lh(labs)
-    freq1 = reduce3_to_bci_lh(freq1)
-    bci_labs_orig = bci_labs
+#    bci_labs = reduce3_to_bci_lh(labs)
+#    freq1 = reduce3_to_bci_lh(freq1)
+    bci_labs = labs.squeeze().copy()
+    bci_labs_orig = bci_labs.copy()
 
     if sp.amax(bci_labs) > 0:
         bci_labs[bci_bst.labels != roino*10] = 0
@@ -79,31 +79,59 @@ bci_bst.attributes = freq
 bci_bst = patch_color_labels(bci_bst, cmap='Paired')
 # bci_bst = smooth_patch(bci_bst, iterations=90, relaxation=10.8)
 view_patch_vtk(bci_bst, show=1)
-writedfs('/home/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
-mid.cortex_refined_labs_uncorr.dfs', bci_bst)
-bci_bst = patch_color_attrib(bci_bst, bci_bst.labels)
+####writedfs('/home/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
+####mid.cortex_refined_labs_uncorr.dfs', bci_bst)
+bci_bst = patch_color_attrib(bci_bst, bci_bst.attributes)
 view_patch_vtk(bci_bst, show=1)
 
 bci_bst = patch_color_labels(bci_bst, freq=freq, cmap='Paired')
 # bci_bst = smooth_patch(bci_bst, iterations=90, relaxation=10.8)
 view_patch_vtk(bci_bst, show=1)
-writedfs('/home/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
+####writedfs('/home/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
+####mid.cortex_refined_labs_mod_freq_uncorr.dfs', bci_bst)
+bci_bst = patch_color_attrib(bci_bst, bci_bst.labels)
+view_patch_vtk(bci_bst, show=1)
+
+bci_labs = reduce3_to_bci_lh(bci_bst.labels)
+bci_freq = reduce3_to_bci_lh(bci_bst.attributes)
+
+bci_bst = readdfs('/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
+mid.cortex.dfs')
+bci_bst_sm = readdfs('/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.\
+left.mid.cortex_smooth10.dfs')
+bci_bst.vertices = bci_bst_sm.vertices
+
+bci_bst.labels = bci_labs
+bci_bst.attributes = bci_freq
+
+bci_bst = patch_color_labels(bci_bst, cmap='Paired')
+view_patch_vtk(bci_bst, show=1)
+writedfs('/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
+mid.cortex_refined_labs_uncorr.dfs', bci_bst)
+
+bci_bst = patch_color_labels(bci_bst, freq=bci_bst.attributes, cmap='Paired')
+# bci_bst = smooth_patch(bci_bst, iterations=90, relaxation=10.8)
+view_patch_vtk(bci_bst, show=1)
+writedfs('/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
 mid.cortex_refined_labs_mod_freq_uncorr.dfs', bci_bst)
 
-surfname = '/home/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
+
+
+
+surfname = '/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
 mid.cortex_refined_labs_uncorr.dfs'
-sub_out = '/home/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
+sub_out = '/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
 mid.cortex_refined_labs_out.dfs'
 
 eng = meng.start_matlab()
-eng.addpath(eng.genpath('/home/ajoshi/coding_ground/svreg-matlab/MEX_Files'))
-eng.addpath(eng.genpath('/home/ajoshi/coding_ground/svreg-matlab/3rdParty'))
-eng.addpath(eng.genpath('/home/ajoshi/coding_ground/svreg-matlab/src'))
+eng.addpath(eng.genpath('/big_disk/ajoshi/coding_ground/svreg-matlab/MEX_Files'))
+eng.addpath(eng.genpath('/big_disk/ajoshi/coding_ground/svreg-matlab/3rdParty'))
+eng.addpath(eng.genpath('/big_disk/ajoshi/coding_ground/svreg-matlab/src'))
 
 eng.corr_topology_labels(surfname, sub_out)
 
 bci_bst = readdfs(sub_out)
-bci_bst = patch_color_labels(bci_bst, freq=freq, cmap='Paired')
+bci_bst = patch_color_labels(bci_bst, freq=bci_bst.attributes, cmap='Paired')
 # bci_bst = smooth_patch(bci_bst, iterations=90, relaxation=10.8)
 view_patch_vtk(bci_bst, show=1)
 writedfs('/big_disk/ajoshi/data/BCI-DNI_brain_atlas/BCI-DNI_brain.left.\
